3e2cca4995b926b75d0a2402fdd755197b0a9d23
jergolier <tonmailgithub@example.comjargolliercedric@gmail.com>
2026-03-30 19:02:11 +0200

    page minimale

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
 export default function Home() {
-  return (
-    <main
-      style={{
-        width: "100vw",
-        height: "100vh",
-        backgroundImage: "url('/truck.jpg')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        margin: 0,
-      }}
-    />
-  );
+  return <main>OK</main>;
 }

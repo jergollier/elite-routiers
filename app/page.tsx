6696c1be2c01df76451a457653816ef5f31ead46
jergolier <tonmailgithub@example.comjargolliercedric@gmail.com>
2026-03-30 15:42:23 +0200

    fix background homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main
       style={{
+        width: "100vw",
         height: "100vh",
-        margin: 0,
         backgroundImage: "url('/truck.jpg')",
         backgroundSize: "cover",
         backgroundPosition: "center",

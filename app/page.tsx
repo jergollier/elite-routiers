29f6467def28c6e93b226641a02ad3e3554d2745
jergolier <tonmailgithub@example.comjargolliercedric@gmail.com>
2026-03-30 18:54:35 +0200

    use local imported background

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
+import background from "./truck.jpg";
+
 export default function Home() {
   return (
     <main
       style={{
         width: "100vw",
         height: "100vh",
-        backgroundImage: "url('/truck.jpg')",
+        backgroundImage: `url(${background.src})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
+        margin: 0,
       }}
     />
   );

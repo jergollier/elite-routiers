64edfc0d343c41f56a5fe278e59003048cef919c
jergolier <tonmailgithub@example.comjargolliercedric@gmail.com>
2026-03-30 15:51:28 +0200

    test vercel rouge

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,19 @@
-import Image from "next/image";
-import fondsite from "./fondsite.jpg";
-
 export default function Home() {
   return (
     <main
       style={{
-        position: "relative",
         width: "100vw",
         height: "100vh",
-        overflow: "hidden",
+        background: "red",
+        color: "white",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "60px",
+        fontWeight: "bold",
       }}
     >
-      <Image
-        src={fondsite}
-        alt="Fond du site"
-        fill
-        priority
-        style={{
-          objectFit: "cover",
-          objectPosition: "center",
-        }}
-      />
+      TEST VERCEL ROUGE
     </main>
   );
 }

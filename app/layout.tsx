83bb7f25d97856f1908e7c5aef3fb0e102bb9a55
jergolier <tonmailgithub@example.comjargolliercedric@gmail.com>
2026-03-30 20:02:16 +0200

    reset clean final

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "Elite Routiers",
-  description: "Elite Routiers",
-};
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="fr">
       <body>{children}</body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main>OK</main>;
+  return <main>RESET OK</main>;
 }

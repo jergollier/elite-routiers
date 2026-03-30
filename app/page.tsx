7918ab2b3cf7d6b2116bae554a54304ae82de415
jergolier <tonmailgithub@example.comjargolliercedric@gmail.com>
2026-03-30 15:39:56 +0200

    fond ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 export default function Home() {
   return (
-    <main style={{
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      backgroundImage: "url('/truck.jpg')",
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      color: "white",
-      fontSize: "3rem",
-      fontWeight: "bold"
-    }}>
-      Elite Routiers 🚛🔥
-    </main>
+    <main
+      style={{
+        height: "100vh",
+        margin: 0,
+        backgroundImage: "url('/truck.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    />
   );
 }

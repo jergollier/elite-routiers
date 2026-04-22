48aafd759250d3c7c3bd2f1390ae6bd692f3a49b
jergolier <tonmailgithub@example.comjargolliercedric@gmail.com>
2026-04-22 15:10:11 +0200

    update tacky setup 1.0.3 blob

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -71,11 +71,11 @@ export default async function Menu() {
 
         {/* ✅ TACKY (1 seul bouton) */}
         <a
-          href="/downloads/Elite%20Routier%20Tacky%201.0.3.exe"
-          style={downloadStyle}
-        >
-          ⬇ Télécharger le Tacky
-        </a>
+  href="https://evsucubtev4fgabq.public.blob.vercel-storage.com/tacky/Elite%20Routier%20Tacky%20Setup%201.0.3.exe"
+  style={downloadStyle}
+>
+  ⬇ Télécharger le Tacky
+</a>
 
         {/* ✅ PLUGIN (corrigé) */}
         <a
